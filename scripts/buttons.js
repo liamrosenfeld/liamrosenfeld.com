4fc856917a1144ee8b84d7784643fe4ff48df6fb
Liam Rosenfeld <liamrosenfeld@gmail.com>
2017-12-25 16:00:49 -0500

    Slides Framework

---
+++ scripts/buttons.js
@@ -0,0 +1,31 @@
+// App Slides
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("appSlides");
+  var dots = document.getElementsByClassName("button");
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" clickedButton", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " clickedButton";
+}
