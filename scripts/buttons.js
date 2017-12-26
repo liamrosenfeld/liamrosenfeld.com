6958900e5a80ad19b94ea86ce295ef6cd6a0a099
Liam Rosenfeld <liamrosenfeld@gmail.com>
2017-12-25 21:33:29 -0500

    Slider JS for Website Section without breaking everything

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,31 +1,63 @@
 // App Slides
-var slideIndex = 1;
-showDivs(slideIndex);
+var appSlideIndex = 1;
+showAppDivs(appSlideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusAppDivs(n) {
+  showAppDivs(appSlideIndex += n);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentAppDiv(n) {
+  showAppDivs(appSlideIndex = n);
 }
 
-function showDivs(n) {
+function showAppDivs(n) {
   var i;
   var x = document.getElementsByClassName("appSlides");
-  var dots = document.getElementsByClassName("button");
+  var appDots = document.getElementsByClassName("appButton");
   if (n > x.length) {
-    slideIndex = 1
+    appSlideIndex = 1
   }
   if (n < 1) {
-    slideIndex = x.length
+    appSlideIndex = x.length
   }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" clickedButton", "");
+  for (i = 0; i < appDots.length; i++) {
+    appDots[i].className = appDots[i].className.replace(" clickedButton", "");
   }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " clickedButton";
+  x[appSlideIndex - 1].style.display = "block";
+  appDots[appSlideIndex - 1].className += " clickedButton";
+}
+
+// Website Slides
+var webSlideIndex = 1;
+showWebDivs(webSlideIndex);
+
+function plusWebDivs(m) {
+  showWebDivs(webSlideIndex += m);
+}
+
+function currentWebDiv(m) {
+  showWebDivs(webSlideIndex = m);
+}
+
+function showWebDivs(m) {
+  var j;
+  var y = document.getElementsByClassName("webSlides");
+  var webDots = document.getElementsByClassName("webButton");
+  if (m > y.length) {
+    webSlideIndex = 1
+  }
+  if (m < 1) {
+    webSlideIndex = y.length
+  }
+  for (j = 0; j < y.length; j++) {
+    y[j].style.display = "none";
+  }
+  for (j = 0; j < webDots.length; j++) {
+    webDots[j].className = webDots[j].className.replace(" clickedButton", "");
+  }
+  y[webSlideIndex - 1].style.display = "block";
+  webDots[webSlideIndex - 1].className += " clickedButton";
 }
