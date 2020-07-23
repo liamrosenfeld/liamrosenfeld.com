8b3b96278d72b0560570aa7f6725d7936d980a87
Liam Rosenfeld <me@liamrosenfeld.com>
2020-07-23 18:19:28 -0400

    Improved Project Category Picker
    can take the filter from a browser query
    
    now bootstrap is the only thing that uses jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,9 @@
-// Preloader js    
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
-});
-
-(function ($) {
-	'use strict';
-
-	// Sticky Menu
-	$(window).scroll(function () {
-		if ($('.navigation').offset().top > 100) {
-			$('.navigation').addClass('nav-bg');
-		} else {
-			$('.navigation').removeClass('nav-bg');
-		}
-	});
-
-	// Background-images
-	$('[data-background]').each(function () {
-		$(this).css({
-			'background-image': 'url(' + $(this).data('background') + ')'
-		});
-	});
-
-	// background color
-	$('[data-color]').each(function () {
-		$(this).css({
-			'background-color': $(this).data('color')
-		});
-	});
-
-	// progress bar
-	$('[data-progress]').each(function () {
-		$(this).css({
-			'bottom': $(this).data('progress')
-		});
-	});
-
-	// Shuffle js filter and masonry
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
-
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
+// Sticky Menu
+window.onscroll = () => {
+	let nav = document.getElementsByClassName("navigation")[0];
+	if (window.scrollY > 100) {
+		nav.classList.add('nav-bg');
+	} else {
+		nav.classList.remove('nav-bg');
 	}
-
-
-
-})(jQuery);
+};
