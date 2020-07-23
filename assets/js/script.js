d64baabbd38e0eee38f64fb662c419534090bc14
Liam Rosenfeld <me@liamrosenfeld.com>
2020-07-23 19:49:37 -0400

    Removed Tab Stragglers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Sticky Menu
 window.onscroll = () => {
-	let nav = document.getElementsByClassName("navigation")[0];
-	if (window.scrollY > 100) {
-		nav.classList.add('nav-bg');
-	} else {
-		nav.classList.remove('nav-bg');
-	}
+  let nav = document.getElementsByClassName("navigation")[0];
+  if (window.scrollY > 100) {
+    nav.classList.add('nav-bg');
+  } else {
+    nav.classList.remove('nav-bg');
+  }
 };
