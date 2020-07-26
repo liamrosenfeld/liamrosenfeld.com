0f791e544e44fd7100defac70685fe4cb482ff80
Liam Rosenfeld <me@liamrosenfeld.com>
2020-07-25 22:22:23 -0400

    Remove Bootstrap from Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Sticky Menu
 window.onscroll = () => {
-  let nav = document.getElementsByClassName("navigation")[0];
+  let nav = document.getElementById("navbar");
   if (window.scrollY > 100) {
-    nav.classList.add('nav-bg');
+    nav.classList.add("scrolled");
   } else {
-    nav.classList.remove('nav-bg');
+    nav.classList.remove("scrolled");
   }
 };
