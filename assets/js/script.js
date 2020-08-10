5b7afe9b44c31fe773b1b7559b5b8fdb49d79f78
Liam Rosenfeld <me@liamrosenfeld.com>
2020-08-10 01:23:30 -0400

    Sticky Footer

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Sticky Menu
-window.onscroll = () => {
-  let nav = document.getElementById("navbar");
-  if (window.scrollY > 100) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-};
